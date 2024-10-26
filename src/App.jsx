07b74061228297ac14455c7267eda46e3067598b
StackCanary <danizmd8@gmail.com>
2024-10-26 12:38:09 +0300

    feat: add material ui and remove unnecessary things

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import viteLogo from "/logo.png";
+import { Button } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -28,8 +24,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Button variant="contained">Aboba</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
