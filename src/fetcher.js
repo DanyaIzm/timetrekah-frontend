933e3c3dc54528fbaa9b99e9985f8963c4fd1c6c
StackCanary <danizmd8@gmail.com>
2024-12-17 22:17:29 +0300

    feat: add ability to test from another device
    In the future this link should be concrete and provided by process variable

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,4 +1,4 @@
-const BASE_BACKEND_URL = "http://localhost:8000";
+const BASE_BACKEND_URL = `http://${window.location.hostname}:8000`;
 
 export const fetcher = async (url) => {
   const res = await fetch(BASE_BACKEND_URL + url);
