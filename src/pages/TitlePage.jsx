824c861348ba3433436bfdfd0714963c4ae5388e
StackCanary <danizmd8@gmail.com>
2024-12-18 12:26:43 +0300

    fix: use page loader custom component instead of circularprogress on title page

--- src/pages/TitlePage.jsx
+++ src/pages/TitlePage.jsx
@@ -14,6 +14,7 @@ import useSWR from "swr";
 import { getAuthFetcher } from "../fetcher";
 import AuthContext from "../contexts/auth-context";
 import TitleCard from "../components/TitleCard";
+import PageLoader from "../components/PageLoader";
 
 const TitlePage = () => {
   const [title, setTitle] = useState(null);
@@ -52,7 +53,7 @@ const TitlePage = () => {
     }
   }, [error]);
 
-  if (isLoading || !title) return <CircularProgress />;
+  if (isLoading || !title) return <PageLoader />;
 
   return (
     <Stack
